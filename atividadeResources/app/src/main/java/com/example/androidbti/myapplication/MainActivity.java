91553e4243bbd1f0a8ab1110e4e3ff7d3bad103b
Android BTI <android.bti@A309-10L.imd.local>
2018-03-08 18:23:46 -0300

    començando a trabalhar, nada de especial feito

--- atividadeResources/app/src/main/java/com/example/androidbti/myapplication/MainActivity.java
+++ atividadeResources/app/src/main/java/com/example/androidbti/myapplication/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.androidbti.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtNome = findViewById(R.id.editText);
+        String nome = edtNome.getT
     }
 }
