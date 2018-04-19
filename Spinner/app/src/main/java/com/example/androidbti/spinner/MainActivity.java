9c370f978a25d0a40163aa8affae992a3b5f837c
Android BTI <android.bti@A309-02L.imd.local>
2018-04-19 17:35:54 -0300

    add dialog project

--- Spinner/app/src/main/java/com/example/androidbti/spinner/MainActivity.java
+++ Spinner/app/src/main/java/com/example/androidbti/spinner/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.androidbti.spinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Cria o spiner dentro da activity
+        Spinner spinner = findViewById(R.id.spinner);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        adapter.add("Computador");
+        adapter.add("tv");
+        adapter.add("Monitor");
+        adapter.add("teclado");
+        adapter.add("HD");
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String produto = adapter.getItem(position);
+
+        Toast.makeText(this,"Produto Selecionado "+produto, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        //
     }
 }
