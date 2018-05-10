be32a00d41283c94148f2432a0ae083e026236e8
Leandro Max <leandromaxnatal@gmail.com>
2018-05-10 12:29:57 -0300

    add Exemplo Starbuzz - ListViews e adapters

---
+++ StarbuzzListAndAdapters/app/src/main/java/br/com/leandromax/starbuzzlistandadapers/TopLevel.java
@@ -0,0 +1,13 @@
+package br.com.leandromax.starbuzzlistandadapers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopLevel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_level);
+    }
+}
